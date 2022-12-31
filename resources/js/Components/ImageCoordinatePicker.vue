3f63b933874a409374f163536d45762a162dbda6
Timotej Avsec <avsectimotej1@gmail.com>
2022-12-31 22:19:33 +0100

    Add reformatted coordinates, taking into account scale

--- resources/js/Components/ImageCoordinatePicker.vue
+++ resources/js/Components/ImageCoordinatePicker.vue
@@ -30,7 +30,15 @@
             </div>
             <div class="w-1/4 p-2">
 
-                <p>First, select the area which will be replaced with screenshot. Order of the points is not important.</p>
+                <p>First, select the area which will be replaced with screenshot. When you are satisfied, click save points button. <br/><br/> Then select 4 points, where screenshot will be placed. First select left-top, left-bottom, right-bottom, right-top</p>
+                <div>
+                    <h1 class="font-bold">Points:</h1>
+                    <pre class="overflow-y-auto text-small" style="border: 1px solid black; max-height: 500px">
+{{formattedPoints}}
+                    </pre>
+                    <button @click="savePoints" type="button" class="mt-2 py-2 px-3 mb-1 text-xs font-medium text-center text-white bg-green-700 rounded-lg hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Save points</button>
+
+                </div>
             </div>
         </div>
     </section>
@@ -68,6 +76,16 @@ export default {
                     this.drawPoints()
                 }
         }
+    },
+    computed: {
+      formattedPoints(){
+          return this.points.map((el) => {
+             return {
+                 x: el.x * ((1/ this.scale)),
+                 y: el.y * ((1/ this.scale))
+             }
+          });
+      }
     },
     methods: {
         setFile(event){
@@ -117,6 +135,9 @@ export default {
         clearPoints(){
             this.points = []
             this.redraw()
+        },
+        savePoints(){
+
         }
     }
 }
