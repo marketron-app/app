853a42db2673c0c3447c93d2950f3db274f7a648
Timotej Avsec <avsectimotej1@gmail.com>
2023-02-11 05:52:52 +0100

    Revert "Merge branch 'main-gh'"
    This reverts commit a899a66bdbf77efad7d54f85b2374ee067bf7393, reversing
    changes made to 24ccaf37f44507eb0e2761d57d1266ff25d8e811.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,18 +14,6 @@
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
     </head>
-    @if(config("app.env") === "production" ) 
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-VE8RN30EV9"></script>
-        <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-VE8RN30EV9');
-        </script>
-    @endif
-
     <body class="font-sans antialiased">
         @inertia
     </body>
