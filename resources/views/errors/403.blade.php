029373105c666f81e9f27202dd3f5aabe3da0ef9
Timotej Avsec <avsectimotej1@gmail.com>
2023-02-13 16:25:43 +0100

    Add 404 and 403 error pages

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>404</title>
+    @vite(['resources/js/app.js'])
+
+</head>
+<body>
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
+
+
+    <div
+        class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
+    >
+        <div class="w-full bg-white rounded-lg  md:mt-0 sm:max-w-md xl:p-0 flex flex-col text-center justify-content-center align-items-center">
+            <a href="/“"><img class=" w-auto text-align-center" src="/images/marketron-cropped.png" alt="" /></a>
+
+            <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center">
+                    You don't have access to this page!
+                </h1>
+                <a href="/" class="text-center text-blue-400">Back to the home page</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
