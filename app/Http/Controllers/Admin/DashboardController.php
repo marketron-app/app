89635aa4d95179f18c9f6c42e110d5a03f411d10
Timotej Avsec <avsectimotej1@gmail.com>
2023-02-24 10:34:52 +0100

    Pint refactor

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,16 +8,17 @@ use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
-    public function __construct(private StatisticService $statisticService){
-
+    public function __construct(private StatisticService $statisticService)
+    {
     }
+
     public function index()
     {
         return Inertia::render('Admin/Dashboard', [
-            "totalUsers" => $this->statisticService->getNumberOfTotalUsers(),
-            "newUsers" => $this->statisticService->getNumberOfTotalUsers(now()->subDay(), now()),
-            "totalImages" => $this->statisticService->getNumberOfTotalImages(),
-            "newImages" => $this->statisticService->getNumberOfTotalImages(now()->subDay(), now()),
+            'totalUsers' => $this->statisticService->getNumberOfTotalUsers(),
+            'newUsers' => $this->statisticService->getNumberOfTotalUsers(now()->subDay(), now()),
+            'totalImages' => $this->statisticService->getNumberOfTotalImages(),
+            'newImages' => $this->statisticService->getNumberOfTotalImages(now()->subDay(), now()),
         ]);
     }
 }
--- app/Services/Statistics/StatisticService.php
+++ app/Services/Statistics/StatisticService.php
@@ -9,17 +9,19 @@ use Illuminate\Contracts\Database\Eloquent\Builder;
 
 class StatisticService
 {
-    public function getNumberOfTotalUsers(Carbon|null $timeFrom = null, Carbon|null $timeTo = null){
+    public function getNumberOfTotalUsers(Carbon|null $timeFrom = null, Carbon|null $timeTo = null)
+    {
         return User::query()
-            ->when(!is_null($timeFrom), fn(Builder $q) => $q->whereDate("created_at", ">=", $timeFrom))
-            ->when(!is_null($timeTo), fn(Builder $q) => $q->whereDate("created_at", "<=", $timeTo))
+            ->when(! is_null($timeFrom), fn (Builder $q) => $q->whereDate('created_at', '>=', $timeFrom))
+            ->when(! is_null($timeTo), fn (Builder $q) => $q->whereDate('created_at', '<=', $timeTo))
             ->count();
     }
 
-    public function getNumberOfTotalImages(Carbon|null $timeFrom = null, Carbon|null $timeTo = null){
+    public function getNumberOfTotalImages(Carbon|null $timeFrom = null, Carbon|null $timeTo = null)
+    {
         return Image::query()
-            ->when(!is_null($timeFrom), fn(Builder $q) => $q->whereDate("created_at", ">=", $timeFrom))
-            ->when(!is_null($timeTo), fn(Builder $q) => $q->whereDate("created_at", "<=", $timeTo))
+            ->when(! is_null($timeFrom), fn (Builder $q) => $q->whereDate('created_at', '>=', $timeFrom))
+            ->when(! is_null($timeTo), fn (Builder $q) => $q->whereDate('created_at', '<=', $timeTo))
             ->count();
     }
 }
