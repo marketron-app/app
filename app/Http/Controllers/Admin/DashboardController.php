cf49dfa7665f894b69caf3a9db3ae15db9ce586e
Timotej Avsec <avsectimotej1@gmail.com>
2023-02-24 10:33:32 +0100

    Add new users and new images widget

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,21 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Services\Statistics\StatisticService;
 use Inertia\Inertia;
 
 class DashboardController extends Controller
 {
+    public function __construct(private StatisticService $statisticService){
+
+    }
     public function index()
     {
-        return Inertia::render('Admin/Dashboard');
+        return Inertia::render('Admin/Dashboard', [
+            "totalUsers" => $this->statisticService->getNumberOfTotalUsers(),
+            "newUsers" => $this->statisticService->getNumberOfTotalUsers(now()->subDay(), now()),
+            "totalImages" => $this->statisticService->getNumberOfTotalImages(),
+            "newImages" => $this->statisticService->getNumberOfTotalImages(now()->subDay(), now()),
+        ]);
     }
 }
