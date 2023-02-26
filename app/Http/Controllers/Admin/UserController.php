9e90c9bc999814226d9a15f8e737a7f76f030008
Timotej Avsec <avsectimotej1@gmail.com>
2023-02-26 07:00:47 +0100

    Add pagination and search to users

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\PaginationSearchRequest;
 use App\Http\Resources\Admin\Users\UserResource;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class UserController extends Controller
 {
-    public function index(Request $request)
+    public function index(PaginationSearchRequest $request)
     {
-        $users = UserResource::collection(User::query()->paginate());
+        $users = User::query()
+            ->when(!empty($request->get('search')), function (Builder $query) use ($request){
+                return $query->where(function(Builder $query) use ($request){
+                    return $query->where("email", "LIKE", "%". $request->get("search") ."%");
+                });
+            })
+            ->paginate($request->get("perPage"))->withQueryString();
 
-        return Inertia::render('Admin/User/Index')->with(['users' => $users]);
+        return Inertia::render('Admin/User/Index')->with(['users' => UserResource::collection($users),'perPage' => $users->perPage(), 'search' => $request->get("search")]);
     }
 }
--- resources/js/Pages/Admin/User/Index.vue
+++ resources/js/Pages/Admin/User/Index.vue
@@ -15,6 +15,27 @@ import {format} from "date-fns";
         </template>
 
         <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 mb-3">
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg p-3 flex justify-between">
+                    <form class="w-1/4" method="get">
+                        <label for="default-search" class="mb-2 text-sm font-medium text-gray-900 sr-only ">Search</label>
+                        <div class="relative">
+                            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 " fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                            </div>
+                            <input type="search" :value="search" id="search" name="search"  class="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 " placeholder="Search">
+                            <input hidden="true" :value="perPage" name="perPage" />
+                            <button type="submit" class="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 ">Search</button>
+                        </div>
+                    </form>
+
+                    <select v-model="perPageSelect" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 w-1/6">
+                        <option selected>Per page</option>
+                        <option v-for="x in [10, 15, 20, 50, 75, 100]" :value="x" :key="x">{{ x }}</option>
+                    </select>
+                </div>
+            </div>
+
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                     <table class="w-full text-sm text-left">
@@ -94,8 +115,27 @@ import {format} from "date-fns";
 <script>
 export default {
     name: "Index.vue",
+    data(){
+        return {
+            perPageSelect: 15
+        }
+    },
     props: {
-        users: Object
+        users: Object,
+        perPage: {
+            type: Number,
+            default: 15
+        },
+        search: String
+    },
+    mounted() {
+        this.perPageSelect = this.perPage
+    },
+    watch: {
+        perPageSelect(newVal, oldVal){
+            if(newVal !== this.perPage)
+                this.$inertia.get(route('admin.users.index', {'perPage': newVal, 'search': this.search}))
+        }
     }
 }
 </script>
